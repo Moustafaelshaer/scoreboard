fd4f95e3c616d985d0d57300cb04e7c3dbc2406c
Moustafaelshaer <83034727+Moustafaelshaer@users.noreply.github.com>
2025-09-24 15:04:36 +0300

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let homeEl = document.getElementById("home-el")
+let guestEl = document.getElementById("guest-el")
+
+function add1home(){
+    homeEl.textContent =  Number(homeEl.textContent) +1
+}
+function add2home(){
+    homeEl.textContent =  Number(homeEl.textContent) +2
+}
+function add3home(){
+    homeEl.textContent =  Number(homeEl.textContent) +3
+}
+function add1guest(){
+    guestEl.textContent =  Number(guestEl.textContent) +1
+}
+function add2guest(){
+    guestEl.textContent =  Number(guestEl.textContent) +2
+}
+function add3guest(){
+    guestEl.textContent =  Number(guestEl.textContent) +3
+}
